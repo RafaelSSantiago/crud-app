e8b5263955cfbbb9941fecf36814689c25b01ffe
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-18 18:41:02 -0300

    feat: add tratamento erro

--- backend/src/upload/upload.service.ts
+++ backend/src/upload/upload.service.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import path from 'path';
 import * as fs from 'fs';
 
-
 @Injectable()
 export class UploadService {
-  saveFile(file: Express.Multer.File): string {
-    const uploadPath = path.join(__dirname, '..', '..', 'uploads');
-   
-   if(!fs.existsSync(uploadPath)){
-      fs.mkdirSync(uploadPath);
-    }
+    saveFile(file: Express.Multer.File): string {
+        try {
+            const uploadPath = path.join(__dirname, '..', '..', 'uploads');
 
-    const filePath = path.join(uploadPath, file.originalname);
-    
-    fs.writeFileSync(filePath, file.buffer);
-    return `/uploads/${file.originalname}`;
-  }
+            if (!fs.existsSync(uploadPath)) {
+                fs.mkdirSync(uploadPath, { recursive: true });
+            }
+
+            const filePath = path.join(uploadPath, file.originalname);
+
+            fs.writeFileSync(filePath, file.buffer);
+            return `/uploads/${encodeURIComponent(file.originalname)}`;
+        } catch (error) {
+            console.error('Erro ao salvar o arquivo:', error);
+            throw new InternalServerErrorException('Falha ao salvar o arquivo.');
+        }
+    }
 }
