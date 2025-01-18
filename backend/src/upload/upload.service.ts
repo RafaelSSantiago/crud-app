561faba54b42f723e0a355e226a6d8d08efabdcd
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-18 14:05:46 -0300

    feat: upload servicie

--- backend/src/upload/upload.service.ts
+++ backend/src/upload/upload.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import path from 'path';
+import * as fs from 'fs';
+
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  saveFile(file: Express.Multer.File): string {
+    const uploadPath = path.join(__dirname, '..', '..', 'uploads');
+   
+   if(!fs.existsSync(uploadPath)){
+      fs.mkdirSync(uploadPath);
+    }
+
+    const filePath = path.join(uploadPath, file.originalname);
+    
+    fs.writeFileSync(filePath, file.buffer);
+    return `/uploads/${file.originalname}`;
+  }
+}
