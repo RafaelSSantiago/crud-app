7a216d5c15cbb9af13338df824f7fa910b356ac8
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-18 12:05:18 -0300

    feat: add module upload

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemModule } from './item/item.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/itens'), ItemModule]
+    imports: [MongooseModule.forRoot('mongodb://localhost/itens'), ItemModule, UploadModule]
 })
 export class AppModule {}
