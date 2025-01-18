8e04c0f39fa143c4a4979b7a35ca952512da400d
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-18 14:05:28 -0300

    feat: upload controller

--- backend/src/upload/upload.controller.ts
+++ backend/src/upload/upload.controller.ts
@@ -1,16 +1,20 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
-
+import { multerOptions } from './utils/upload.utils';
 
 @Controller('upload')
 export class UploadController {
-  constructor(private readonly uploadService: UploadService){}
+    constructor(private readonly uploadService: UploadService) {}
 
     @Post()
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('file', multerOptions()))
     async uploadFile(@UploadedFile() file: Express.Multer.File) {
-      const fileUrl = this.uploadService.saveFile(file);
-      return {url: fileUrl}
+        if (!file) {
+            throw new BadRequestException('Nenhum arquivo foi enviado.');
+        }
+
+        const fileUrl = this.uploadService.saveFile(file);
+        return { url: fileUrl };
     }
 }
