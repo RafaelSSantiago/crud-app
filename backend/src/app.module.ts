af3dafac0261d2ec99317e3c3b6eb7b8dd4ce7cf
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-18 13:34:26 -0300

    feat: configurando serve static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ItemModule } from './item/item.module';
 import { UploadModule } from './upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [MongooseModule.forRoot('mongodb://localhost/itens'), ItemModule, UploadModule]
+    imports: [
+        MongooseModule.forRoot('mongodb://localhost/itens'),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'uploads'),
+            serveRoot: '/uploads'
+        }),
+        ItemModule,
+        UploadModule
+    ]
 })
 export class AppModule {}
