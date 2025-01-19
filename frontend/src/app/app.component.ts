ba657f6b3699fa2bafa928ffc993536f2e633e84
RafaelSSantiago <raphasantiago768@gmail.com>
2025-01-19 12:46:23 -0300

    feat: downgrade

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MATERIAL_IMPORTS } from './material.imports';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatListModule,
-    ...MATERIAL_IMPORTS,
-    // Importe outros módulos do Angular Material conforme necessário
-  ],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CRUD App';
-  showFiller = false;
+  title = 'meu-projeto';
 }
--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,20 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { MATERIAL_IMPORTS } from '../material.imports';
-import { ItemListComponent } from './components/item-list/item-list.component';
-import { MatDrawer } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  imports: [...MATERIAL_IMPORTS, ItemListComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss',
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-  @ViewChild('drawer') drawer!: MatDrawer;
 
-  showFiller = false;
-
-  toggleDrawer() {
-    this.drawer.toggle();
-  }
 }
